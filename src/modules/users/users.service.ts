bf8d0d9f72c997f960cc7aadc50b3668a56328e5
namhngo <nathanngo010203@gmail.com>
2025-07-27 12:47:06 -0400

    delete user logic

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -116,5 +116,27 @@ export class UsersService {
       }
   }
 
-  // async deleteUser
+  // DELETE user
+  async deleteUser(id: number): Promise<string> {
+      try {
+          // find user by id. If not found, throw error
+          const user = await this.prisma.user.findUniqueOrThrow({
+          where: { id },
+          });
+
+          // delete user using prisma client
+          await this.prisma.user.delete({
+            where: { id },
+          });
+
+          return `User with id ${user.id} deleted`;
+      } catch (error) {
+          // check if user not found and throw error
+          if (error.code === 'P2025') {
+              throw new NotFoundException(`User with id ${id} not found`);
+          }
+          // throw error if any
+          throw new HttpException(error, 500);
+      }
+  }
 }
