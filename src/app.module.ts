9229e9abb9362443f27bdc4ba9a8309c0e7c2c34
namhngo <nathanngo010203@gmail.com>
2025-07-26 22:53:06 -0400

    prisma/postgres setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { UsersModule } from './modules/users/users.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [],
+  imports: [ CoreModule], // add UsersModule later
   controllers: [AppController],
   providers: [AppService],
 })
